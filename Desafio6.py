0b397d525564f8b77d9be51c2ee366c267f41557
augus(Desktop) <augusto_casotti@hotmail.com>
2025-04-21 20:42:12 -0300

    feito mais um exercicio

--- Desafio6.py
+++ Desafio6.py
@@ -1,1 +1,7 @@
-#faça um  scriptque leia o numero  e mostre seu dobro triplo e rais quadrada
+#faça um  script que leia o numero  e mostre seu dobro triplo e rais quadrada
+
+a=int(input('Informe o número que você quer:'))
+a1=a**2
+a2=a**3
+a3=int(a**(1/2))
+print('O número informado é {} seu dobro é {} ,o triplo é {},a raiz quadrada é {}'.format(a,a1,a2,a3))

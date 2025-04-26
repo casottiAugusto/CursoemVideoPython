3528a226d975d7b0515ce73cbf2a63fec13459f3
Joao casotti Notebook <kiritokumomega@gmail.com>
2025-04-26 11:26:06 -0300

    Mandado os desafios para outra maquina. Feito mais exercicios

--- Desafio7.py
+++ Desafio7.py
@@ -1,1 +1,5 @@
 #faça um programa que que leia as duas notas e mostre a media
+n1=int(input('Informe o numero:'))
+n2=int(input('Informe o numero: '))
+soma=(n1+n2)/2
+print('A medía e dos valores {},{} é: {}'.format(n1,n2,soma))
--- Desafio8.py
+++ Desafio8.py
@@ -1,1 +1,4 @@
 #faça um programa que receba o valor em metros e mostre o vamor comvertido em centimetros e milimetros
+n=float(input('Informe o número:'))
+print('O valor em metro   é {} : {} centimentos'.format(n,(n*100)))
+print('O valor em metors é {}: melimetos: {}'.format(n,(n*1000)))

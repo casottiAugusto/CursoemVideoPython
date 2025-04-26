8559b1151d1ff0a7a90dd6df47e3a7aa1debcc61
Joao casotti Notebook <kiritokumomega@gmail.com>
2025-04-26 15:46:38 -0300

    Mandado os desafios para outra maquina. Feito mais exercicios

--- Desafio11.py
+++ Desafio11.py
@@ -1,2 +1,7 @@
 #faça um programa que leia a altura e largura da parede ,ecalcule a area,e calcule
 #a quantidade de tinta para pintala ,lebrando que um balde de tinta pinta uma area de 2m²
+comp=float(input('Informe e altura: '))
+lag=float(input('Informe e largura: '))
+area=comp*lag
+print('A Aréa total é de :{}m²'.format(area))
+print('Para pintar tudo sera necessario {} latas  tinta'.format(int(area/2)))
--- Desafio9.py
+++ Desafio9.py
@@ -1,1 +1,13 @@
 #faça um programa que leia o numero e faça a tabuade dele
+n=int(input('Informeo numero quer quer mutilicar:'))
+print('Multiplique por {} *1 = {}'.format(n,(n*1)))
+print('Multiplique por {} *2={}'.format(n,(n*2)))
+print('Multiplique por {} *3={}'.format(n,(n*3)))
+print('Multiplique por {} *4={}'.format(n,(n*4)))
+print('Multiplique por {} *5={}'.format(n,(n*5)))
+print('Multiplique por {} *6={}'.format(n,(n*6)))
+print('Multiplique por {} *7={}'.format(n,(n*7)))
+print('Multiplique por {} *8={}'.format(n,(n*8)))
+print('Multiplique por {} *9={}'.format(n,(n*9)))
+print('Multiplique por {} *10={}'.format(n,(n*10)))
+
